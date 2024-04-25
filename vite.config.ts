2b726a4a0025b7c22da5fcfc5355202947c9e26f
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-04-24 22:23:17 -0600

    Añadir alias para importacion de archivos

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      pages: "/src/pages",
+      components: "/src/components",
+      routes: "/src/routes",
+      helpers: "/src/helpers",
+      hooks: "/src/hooks",
+      state: "/src/state",
+      types: "/src/types",
+    },
+  },
+});
