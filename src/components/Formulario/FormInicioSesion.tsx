9c8de9b07dcf93ee551acdedb8429959fcbe7ca3
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-05-03 22:03:02 -0600

    chore: Refactorizar formulario de inicio de sesión y agregar validación de campos

--- src/components/Formulario/FormInicioSesion.tsx
+++ src/components/Formulario/FormInicioSesion.tsx
@@ -1,21 +1,107 @@
 import { TituloBienvenida } from "components/ui/TituloBienvenida";
-import { InputGroup } from "./InputGroup";
 import { Link } from "react-router-dom";
 import { Boton } from "components/ui/Botones/Boton";
+import { Label } from "components/ui/Label";
+import { Input } from "components/ui/Input";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { FormInicioSesionType } from "types/index";
+import { loginRequest, profileRequest } from "helpers/api/auth";
+import { useAuthStore } from "../../store/auth";
 
-export const FormInicioSesion = () => {
+export const FormInicioSesion = forwardRef(() => {
+  const setToken = useAuthStore((state) => state.setToken);
+  const setProfile = useAuthStore((state) => state.setProfile);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInicioSesionType>();
+
+  const nombreRef = useRef(null);
+  const contrasenaRef = useRef(null);
+
+  const { ref, ...rest } = register("nombre", { required: true });
+  const { ref: refContrasena, ...restContrasena } = register("contraseña", {
+    required: true,
+  });
+
+  useImperativeHandle(ref, () => nombreRef.current);
+  useImperativeHandle(refContrasena, () => contrasenaRef.current);
+
+  const onSubmit: SubmitHandler<FormInicioSesionType> = async ({
+    nombre,
+    contraseña,
+  }) => {
+    const resLogin = await loginRequest(nombre, contraseña);
+    setToken(resLogin.data.access_token);
+
+    const {
+      data: {
+        data: [profile],
+      },
+    } = await profileRequest();
+    setProfile(profile);
+  };
+
   return (
-    <article className="flex w-1/2 rounded-r-[72px] bg-blanco py-10 shadow-2xl">
-      <article className="mx-auto flex w-10/12 flex-col justify-between">
-        <TituloBienvenida descripcion="Ingrese a su cuenta para continuar" />
-        <InputGroup
-          datosRegistro={[
-            { usuario: "usuario", icon: "mdi:user", type: "text" },
-            { contraseña: "contraseña", icon: "uis:padlock", type: "password" },
-          ]}
-        />
+    <article className="flex w-1/2 flex-col rounded-r-[72px] bg-blanco py-10 shadow-2xl">
+      <TituloBienvenida descripcion="Ingrese a su cuenta para continuar" />
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto flex w-10/12 flex-grow flex-col justify-between"
+      >
+        <div className="flex flex-grow flex-col justify-evenly">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="nombre">Nombre</Label>
+            <Input
+              id="nombre"
+              placeholder="Ingrese su nombre"
+              type="text"
+              {...rest}
+              aria-invalid={errors.nombre ? "true" : "false"}
+              ref={nombreRef}
+            >
+              <Icon icon="mdi:user" className="size-5 text-azulFuerte" />
+            </Input>
+            {errors.nombre?.type === "required" && (
+              <p
+                role="alert"
+                className="text-sm font-medium italic text-red-600"
+              >
+                Este campo es requerido
+              </p>
+            )}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="contraseña">Contraseña</Label>
+            <Input
+              id="contraseña"
+              placeholder="Ingrese su contraseña"
+              type="password"
+              {...restContrasena}
+              aria-invalid={errors.contraseña ? "true" : "false"}
+              ref={contrasenaRef}
+            >
+              <Icon icon="uis:padlock" className="size-5 text-azulFuerte" />
+            </Input>
+            {errors.contraseña?.type === "required" && (
+              <p
+                role="alert"
+                className="text-sm font-medium italic text-red-600"
+              >
+                Este campo es requerido
+              </p>
+            )}
+          </div>
+        </div>
+
         <div className="flex w-1/2 flex-col gap-12 self-center">
-          <Boton />
+          <Boton type="submit" />
           <small className="self-center text-sm font-medium text-azulFuerte">
             ¿No tiene una cuenta?{" "}
             <Link to="/registro" className="text-naranja">
@@ -23,7 +109,7 @@ export const FormInicioSesion = () => {
             </Link>
           </small>
         </div>
-      </article>
+      </form>
     </article>
   );
-};
+});
--- src/components/Formulario/FormRegistro.tsx
+++ src/components/Formulario/FormRegistro.tsx
@@ -1,22 +1,141 @@
+import { Label } from "components/ui/Label";
 import { TituloBienvenida } from "../ui/TituloBienvenida";
 import { BotonGroup } from "./BotonGroup";
-import { InputGroup } from "./InputGroup";
+import { Input } from "components/ui/Input";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { FormRegistroType } from "types/index";
 
-export const FormRegistro = () => {
+export const FormRegistro = forwardRef(() => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormRegistroType>();
+
+  const nombreRef = useRef(null);
+  const usuarioRef = useRef(null);
+  const correoRef = useRef(null);
+  const contrasenaRef = useRef(null);
+
+  const { ref: refNombre, ...rest } = register("nombre", { required: true });
+  const { ref: refUsuario, ...restUsuario } = register("usuario", {
+    required: true,
+  });
+  const { ref: refCorreo, ...restCorreo } = register("correo", {
+    required: true,
+  });
+  const { ref: refContrasena, ...restContrasena } = register("contraseña", {
+    required: true,
+  });
+
+  useImperativeHandle(refNombre, () => nombreRef.current);
+  useImperativeHandle(refUsuario, () => usuarioRef.current);
+  useImperativeHandle(refCorreo, () => correoRef.current);
+  useImperativeHandle(refContrasena, () => contrasenaRef.current);
+
+  const onSubmit: SubmitHandler<FormRegistroType> = (data) => {
+    console.log(data);
+  };
+  console.log(errors);
   return (
-    <article className="flex w-1/2 rounded-r-[72px] bg-blanco py-10 shadow-2xl">
-      <article className="mx-auto flex w-10/12 flex-col justify-between">
-        <TituloBienvenida descripcion="Crea una cuenta para iniciar sesión" />
-        <InputGroup
-          datosRegistro={[
-            { nombre: "nombre", icon: "mdi:user", type: "text" },
-            { usuario: "usuario", icon: "mdi:user", type: "text" },
-            { correo: "correo", icon: "ic:baseline-email", type: "text" },
-            { contraseña: "contraseña", icon: "uis:padlock", type: "password" },
-          ]}
-        />
+    <article className="flex w-1/2 flex-col justify-between rounded-r-[72px] bg-blanco py-10 shadow-2xl">
+      <TituloBienvenida descripcion="Crea una cuenta para iniciar sesión" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto flex w-10/12 flex-grow flex-col"
+      >
+        <div className="flex flex-grow flex-col justify-center">
+          <div className="flex flex-grow flex-col justify-evenly">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="nombre">Nombre</Label>
+              <Input
+                id="nombre"
+                placeholder="Ingrese su nombre"
+                type="text"
+                {...rest}
+                aria-invalid={errors.nombre ? "true" : "false"}
+                ref={nombreRef}
+              >
+                <Icon icon="mdi:user" className="size-5 text-azulFuerte" />
+              </Input>
+              {errors.nombre?.type === "required" && (
+                <p
+                  role="alert"
+                  className="text-sm font-medium italic text-red-600"
+                >
+                  Este campo es requerido
+                </p>
+              )}
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="contraseña">Usuario</Label>
+              <Input
+                id="contraseña"
+                placeholder="Ingrese su usuario"
+                type="text"
+                {...restUsuario}
+                ref={usuarioRef}
+              >
+                <Icon icon="mdi:user" className="size-5 text-azulFuerte" />
+              </Input>
+              {errors.usuario?.type === "required" && (
+                <p
+                  role="alert"
+                  className="text-sm font-medium italic text-red-600"
+                >
+                  Este campo es requerido
+                </p>
+              )}
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="contraseña">Correo</Label>
+              <Input
+                id="contraseña"
+                placeholder="Ingrese su correo"
+                type="text"
+                {...restCorreo}
+                ref={correoRef}
+              >
+                <Icon icon="mdi:email" className="size-5 text-azulFuerte" />
+              </Input>
+              {errors.correo?.type === "required" && (
+                <p
+                  role="alert"
+                  className="text-sm font-medium italic text-red-600"
+                >
+                  Este campo es requerido
+                </p>
+              )}
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="contraseña">Contraseña</Label>
+              <Input
+                id="contraseña"
+                placeholder="Ingrese su contraseña"
+                type="password"
+                {...restContrasena}
+                ref={contrasenaRef}
+              >
+                <Icon icon="uis:padlock" className="size-5 text-azulFuerte" />
+              </Input>
+              {errors.contraseña?.type === "required" && (
+                <p
+                  role="alert"
+                  className="text-sm font-medium italic text-red-600"
+                >
+                  Este campo es requerido
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
         <BotonGroup />
-      </article>
+      </form>
     </article>
   );
-};
+});
