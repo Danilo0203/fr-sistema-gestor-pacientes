6375485162782260f87e14fb119dcc36eba84f5b
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-05-03 22:02:51 -0600

    chore: Añadir botón de regresar en BotonGroup.tsx

--- src/components/Formulario/BotonGroup.tsx
+++ src/components/Formulario/BotonGroup.tsx
@@ -1,11 +1,18 @@
 import { Boton } from "components/ui/Botones/Boton";
 import { BotonRelleno } from "components/ui/Botones/BotonRelleno";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const BotonGroup = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const onClickRegresar = () => {
+    navigate(pathname === "/registro" ? "/inicio-sesion" : "/registro");
+  };
+
   return (
     <div className="flex w-full items-center gap-5 self-center pt-4">
-      <Boton />
-      <BotonRelleno />
+      <Boton onClick={onClickRegresar}> Regresar </Boton>
+      <BotonRelleno type="submit" />
     </div>
   );
 };
