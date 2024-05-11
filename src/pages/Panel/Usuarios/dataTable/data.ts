0399f782c9bc393928dc69aa5e0325be11d2e823
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-05-10 19:25:33 -0600

    chore: Agregar archivo de datos para la tabla de usuarios

---
+++ src/pages/Panel/Usuarios/dataTable/data.ts
@@ -0,0 +1,31 @@
+export const columns = [
+  // {
+  //   key: "id",
+  //   label: "#",
+  //   sortable: true,
+  // },
+  {
+    key: "usuario",
+    label: "Usuario",
+    sortable: true,
+  },
+  {
+    key: "nombre",
+    label: "Nombre",
+    sortable: true,
+  },
+  {
+    key: "email",
+    label: "Correo",
+    sortable: true,
+  },
+  {
+    key: "rol",
+    label: "Rol",
+    sortable: true,
+  },
+  {
+    key: "acciones",
+    label: "Acciones",
+  },
+];
