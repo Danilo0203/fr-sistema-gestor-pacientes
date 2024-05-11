93834f8e1b15cc0d22aad473c592c1c447a06a50
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-05-10 19:24:40 -0600

    chore: Agregar componente Panel a la página con funcionalidad básica

---
+++ src/pages/Panel/Panel.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useUsuarioStore } from "../../store/usuarios";
+
+export const Panel = () => {
+  // const usuarios = useUsuarioStore((state) => state.execute);
+  // const dataUsuarios = useUsuarioStore((state) => state.data);
+  // useEffect(() => {
+  //   usuarios();
+  // }, [usuarios]);
+  // console.log(dataUsuarios);
+
+  return <div>Hola</div>;
+};
