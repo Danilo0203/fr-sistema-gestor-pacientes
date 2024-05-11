399ab5eab1ce7087aa99d967428032b50dc434b4
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-05-10 19:27:06 -0600

    chore: Refactorizar función capitalizarStrings para mejorar rendimiento

--- src/utils/capitalizarStrings.ts
+++ src/utils/capitalizarStrings.ts
@@ -1,5 +1,3 @@
 export const capitalizar = (str: string) => {
-  return str.replace(/\w\S*/g, function (txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
+  return str.charAt(0).toUpperCase() + str.slice(1);
 };
